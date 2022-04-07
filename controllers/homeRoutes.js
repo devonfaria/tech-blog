5de0c683b984572efeb4f8e9f7628ed8f5d942fb
Devon Faria <devonfaria@gmail.com>
2022-04-07 18:18:13 -0400

    added homepage route and handlebars template

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,16 @@ const Post = require('../models/Post');
 const router = require('express').Router();
 const serialize = require('../utils/serialize');
 
+// Loads Homepage
+router.get('/', async (req, res) => {
+  try {
+    res.render('homepage');
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 // Loads Posts page
 router.get('/posts', async (req, res) => {
   try {
