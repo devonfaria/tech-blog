b07da402971a9d7c39aa8758efcf49ddc8e69a6b
Devon Faria <devonfaria@gmail.com>
2022-04-07 19:00:31 -0400

    added signup and login js files for handlebars to load upon request

---
+++ public/js/signup.js
@@ -0,0 +1,23 @@
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.getElementById('username-signup').value.trim();
+  const email = document.getElementById('email-signup').value.trim();
+  const password = document.getElementById('password-signup').value.trim();
+  console.log(username, email, password);
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ username, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to sign up.');
+    }
+  }
+};
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
